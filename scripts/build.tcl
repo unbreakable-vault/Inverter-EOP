4c29347603328a7a951fe0b877d54971933adfd2
unbreakable-vault <mcetin@wpi.edu>
2025-09-05 23:01:11 -0400

    Create build.tcl

---
+++ scripts/build.tcl
@@ -0,0 +1,10 @@
+open_project ../vivado/inverter_skew/inverter_skew.xpr
+reset_run synth_1
+launch_runs synth_1 -jobs 8
+wait_on_run synth_1
+
+reset_run impl_1
+launch_runs impl_1 -to_step write_bitstream -jobs 8
+wait_on_run impl_1
+
+puts "Bitstream written. Check the vivado/inverter_skew tree."
