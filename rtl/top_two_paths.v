20a3aa32a4140648c4ecdbe4dd51c8658e5be3ce
unbreakable-vault <mcetin@wpi.edu>
2025-09-19 12:16:43 -0400

    Update top_two_paths.v

--- rtl/top_two_paths.v
+++ rtl/top_two_paths.v
@@ -1,5 +1,5 @@
 module two_chains(
-  input  wire sys_clk,        // test stimulus inside FPGA
+  input  wire sys_clk,        //  test stimulus inside FPGA
   output wire output_A,      //  chain outputs (internal nets)
   output wire output_B,     //  chain outputs (internal nets)
   output wire output_XOR   //  chain outputs (internal nets)
@@ -7,7 +7,7 @@ module two_chains(
 
  // Keep instances separate; prevents merging or resource sharing.
    (* DONT_TOUCH="true" *) inv3_chain A (.din(sys_clk), .dout(output_A));
-   (* DONT_TOUCH="true" *)  inv3_chain B (.din(sys_clk), .dout(output_B));
+   (* DONT_TOUCH="true" *) inv3_chain B (.din(sys_clk), .dout(output_B));
    (* DONT_TOUCH="true" *) assign output_XOR = output_B ^ output_A ;
   
 endmodule
